660b0a2eb600afa9dba8b36b7404965f2451ff7b
Shivani Yadav <yadashiv@sheridancollege.ca>
2020-02-04 06:38:05 +0530

    adding new files

--- beni_fan_page/settings.py
+++ beni_fan_page/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'rs$6v!d0mp67ptj_0+62qsi5q0&4_g#tp#4!r2juncmhfh5qc+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
