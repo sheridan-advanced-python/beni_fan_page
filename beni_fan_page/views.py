616d4221c083ea32051e0111d20115fb695059cf
Shivani Yadav <yadashiv@sheridancollege.ca>
2020-02-04 08:34:01 +0530

    added doc string and updated travis file

--- beni_fan_page/views.py
+++ beni_fan_page/views.py
@@ -1,7 +1,8 @@
 # bani_fan_page/views.py
-
+"""adding text to my bolg"""
 from django.http import HttpResponse
 
 
 def index(request):
+    """adding text to my page"""
     return HttpResponse("Hello world! Welcome to Beni's Fan Page")
