c80356bcf62ce4910db52f9edfc03a8770ec3e67
Shivani Yadav <yadashiv@sheridancollege.ca>
2020-02-05 06:57:43 +0530

    testing fixing the travis ci error

--- tests/beni_fan_page/test_views.py
+++ tests/beni_fan_page/test_views.py
@@ -1,9 +1,10 @@
 """ test_views.py"""
-
+from django.test import Client
 def test_index_ok(client):
     """ testing to get 200 server response"""
     # Make a GET request to / and store the response object
     # using the Django test client.
+    client = Client()
     response = client.get('/')
     # Assert that the status_code is 200 (OK)
     assert response.status_code == 200
